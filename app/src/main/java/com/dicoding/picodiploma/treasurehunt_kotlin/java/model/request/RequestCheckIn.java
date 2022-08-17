02e4d920815be7789c4fb8f40fc683431b6cd752
niel9537 <niel.zebua8@gmail.com>
2022-08-17 23:37:58 +0700

    update tengah malam

---
+++ app/src/main/java/com/dicoding/picodiploma/treasurehunt_kotlin/java/model/request/RequestCheckIn.java
@@ -0,0 +1,9 @@
+package com.dicoding.picodiploma.treasurehunt_kotlin.java.model.request;
+
+public class RequestNextFlow {
+    final String flow_id;
+
+    public RequestNextFlow(String flow_id) {
+        this.flow_id = flow_id;
+    }
+}
---
+++ app/src/main/java/com/dicoding/picodiploma/treasurehunt_kotlin/java/model/response/checkprogress/CekProgressModel.java
@@ -0,0 +1,2 @@
+package com.dicoding.picodiploma.treasurehunt_kotlin.java.model.response.checkprogress;public class CekProgressModel {
+}
